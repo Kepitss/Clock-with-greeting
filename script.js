c7db0a952ea1911f25069d8cebca38c478165740
Kepitss <59582934+Kepitss@users.noreply.github.com>
2020-11-07 11:49:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Greeting function
+function greeting() {
+    const greet = document.getElementById('greeting');
+    let fullDate = new Date();
+    let hours = fullDate.getHours();
+
+    if (hours >= 0) {
+        greet.innerText = 'Good morning!';
+    }
+    if (hours >= 12) {
+        greet.innerText = 'Good afternoon!';
+    }
+    if (hours >= 18) {
+        greet.innerText= 'Good evening!';
+    }
+}
+
+// Clock function
+function clock() {
+    let fullDate = new Date();
+    let hours = fullDate.getHours();
+    let mins = fullDate.getMinutes();
+    let secs = fullDate.getSeconds();
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+    if (secs < 10) {
+        secs = '0' + secs;
+    }
+
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = ':' + mins;
+    document.getElementById('seconds').innerHTML = ':' + secs;
+
+}
+setInterval(clock, 100);
+
+greeting();
+clock();
